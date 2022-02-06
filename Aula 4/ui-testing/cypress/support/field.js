211b17da295f75ea4baf04e4cb5dc8bd718531da
JoaoVitorVasconcelos <97630186+JoaoVitorVasconcelos@users.noreply.github.com>
2022-02-06 09:17:35 -0300

    aula 4
    organizando o arquivo field

--- Aula 4/ui-testing/cypress/support/field.js
+++ Aula 4/ui-testing/cypress/support/field.js
@@ -6,12 +6,14 @@ const field = {
         technologies: '.App > :nth-child(6)',
         roles: '.App > :nth-child(8)',
     },
+    
     TEXT: {
         skillNameText: '.App > :nth-child(3)',
         developersText: '.App > :nth-child(5)',
         technologiesText: '.App > :nth-child(7)',
         rolesText: '.App > :nth-child(9)',
     },
+
     BOTTON: {
         addSkills: '#load-skills-button',
     }
